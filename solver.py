a76d7f3666ff5453d0983241c5b7b0f9f76aff7b
Jamie Tan <tanjt107@gmail.com>
2021-04-27 00:17:12 +0800

    Changes in recentless function and other minor changes

--- solver.py
+++ solver.py
@@ -4,22 +4,23 @@ import datetime
 import re
 from scipy import optimize
 
-def solver(filepath, home_advantage=True, recentness=True, cut_off_date=None):
+def solver(filepath, home_advantage=True, recent=True, cut_off_date=None, cut_off_number_of_year=None):
 
-    def transformation(df, cut_off_date=None):
+    def transformation(df, cut_off_date=None, cut_off_number_of_year=None):
         # recentness factor gives less weight to games that were played further back in time
-        def calculate_recentness(timestamp, recentness=True):
-            if recentness == True:
-                if cut_off_date == None:
-                    # assume cut-off point is one year
-                    cut_off_timestamp = df.timestamp.max() - 31536000
+        def calculate_recentness(timestamp, recent=True, cut_off_date=None, cut_off_number_of_year=None):
+            if recent == True:
+                if cut_off_date is not None:
+                    cut_off_timestamp = int(datetime.datetime.strptime(cut_off_date, "%d/%m/%Y").timestamp())
+                elif cut_off_number_of_year is not None:
+                    cut_off_timestamp = df.timestamp.max() - cut_off_number_of_year * 31536000
                 else:
-                    cut_off_timestamp = int(datetime.datetime.strptime(cut_off_date, "%b %d %Y").timestamp())
-                # a bonus of up to 25 percent is given to games played within past 25 days to reflect a team's most recent form
+                    cut_off_timestamp = df.timestamp.min()
+                # a bonus of up to 25 percent is given to games played within past past 25 days to reflect a team's most recent form
                 if df.timestamp.max() - timestamp <= 2160000:
                     recentness = (timestamp - cut_off_timestamp) / (df.timestamp.max() - cut_off_timestamp) * (1 + (2628000 - df.timestamp.max() + timestamp) / 2628000 * 0.25)
                 else:
-                    recentness = (timestamp - cut_off_timestamp) / (df.timestamp.max() - cut_off_timestamp)
+                    recentness = max((timestamp - cut_off_timestamp) / (df.timestamp.max() - cut_off_timestamp), 0)
             else:
                 recentness = 1
             return recentness
@@ -77,26 +78,28 @@ def solver(filepath, home_advantage=True, recentness=True, cut_off_date=None):
 
         # average of the two metrics
         def calculate_average_goal(df):
+            # if xg stats are not available
             if (df.team_a_xg == 0) or (df.team_b_xg == 0):
                 df['home_team_average_goal'] = df.home_team_adjusted_goal
                 df['away_team_average_goal'] = df.away_team_adjusted_goal
             else:
+                # take average of two metrics
                 df['home_team_average_goal'] = (df.home_team_adjusted_goal + df.team_a_xg) / 2
                 df['away_team_average_goal'] = (df.away_team_adjusted_goal + df.team_b_xg) / 2
             return df
 
         df = df[df['status'] == 'complete']
         df = df[['timestamp', 'home_team_name', 'away_team_name', 'home_team_goal_count', 'away_team_goal_count', 'home_team_goal_timings', 'away_team_goal_timings', 'team_a_xg', 'team_b_xg']]
-        df['recentness'] = df['timestamp'].apply(calculate_recentness, recentness=recentness)
+        df['recentness'] = df['timestamp'].apply(calculate_recentness, cut_off_date=cut_off_date,cut_off_number_of_year=cut_off_number_of_year)
         df['goal_timings'] = df.apply(get_goal_timings_dict, axis=1)
         df = df.apply(calculate_adjusted_goal, axis=1)
+        df = df.apply(calculate_average_goal, axis=1)
         df['average_goal'] = 'average_goal'
         df['home_advantage'] = 'home_advantage'
         df['home_team_offence'] = df['home_team_name'] + "_offence"
         df['home_team_defence'] = df['home_team_name'] + "_defence"
         df['away_team_offence'] = df['away_team_name'] + "_offence"
         df['away_team_defence'] = df['away_team_name'] + "_defence"
-        df = df.apply(calculate_average_goal, axis=1)
         return df
     
     def get_team_list(df):
@@ -158,10 +161,11 @@ def solver(filepath, home_advantage=True, recentness=True, cut_off_date=None):
     factors = get_factor_array(teams)
     initial = initialise_factors(factors)
     cons = constraints(teams, home_advantage)
-    df = transformation(df, cut_off_date)
+    df = transformation(df, cut_off_date, cut_off_number_of_year)
     solver = optimize.minimize(objective, args=(factors, df), x0=initial, method = 'SLSQP', constraints=cons, options={'maxiter':10000})
     result = pd.DataFrame({'factors': factors, 'values':solver.x})
     return result
 
 filepath = 'england-premier-league-matches-2018-to-2019-stats.csv'
 result = solver(filepath)
+print(result)
