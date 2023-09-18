2908d9cf9feafa3603069139cd09366da49c7cff
Jamie Tan <tanjt107@gmail.com>
2023-09-18 19:47:17 +0800

    Fix sercret modules

--- infrastructure/modules/secret/output.tf
+++ infrastructure/modules/secret/output.tf
@@ -1,6 +1,4 @@
-output "names" {
-  value = concat(
-    values({ for k, v in google_secret_manager_secret.secrets : k => v.name }),
-  )
-  description = "The name list of secrets."
+output "secret_ids" {
+  value       = { for k, v in google_secret_manager_secret.secrets : k => v.secret_id }
+  description = "The id of secrets."
 }
