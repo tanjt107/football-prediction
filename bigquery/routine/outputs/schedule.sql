0d9b563184c8ac8a897fed6d59d3532a8a1e74c0
Jamie Tan <tanjt107@gmail.com>
2023-09-24 11:12:50 +0800

    Refactor code

--- bigquery/routine/outputs/schedule.sql
+++ bigquery/routine/outputs/schedule.sql
@@ -1,20 +1,30 @@
 WITH
-  odds AS (
+  hkjc AS (
   SELECT
     odds_had.matchID,
     odds_had.matchDate,
-    odds_had.homeTeam.teamID AS home_hkjc_id,
-    odds_had.homeTeam.teamNameCH AS home_hkjc_name,
-    odds_had.awayTeam.teamID AS away_hkjc_id,
-    odds_had.awayTeam.teamNameCH AS away_hkjc_name,
+    leagues.transfermarkt_id AS league_transfermarkt_id,
+    home_teams.transfermarkt_id AS home_transfermarkt_id,
+    odds_had.homeTeam.teamNameCH AS home_name,
+    away_teams.transfermarkt_id AS away_transfermarkt_id,
+    odds_had.awayTeam.teamNameCH AS away_name,
+    avg_goal + home_solver.offence + away_solver.defence AS home_exp,
+    avg_goal + away_solver.offence + home_solver.defence AS away_exp,
+    home_adv * CAST(odds_had.venue IS NULL AS INT64) AS home_adv,
     CAST(SPLIT(hadodds.H, '@')[OFFSET(1)] AS FLOAT64) AS had_H,
     CAST(SPLIT(hadodds.D, '@')[OFFSET(1)] AS FLOAT64) AS had_D,
     CAST(SPLIT(hadodds.A, '@')[OFFSET(1)] AS FLOAT64) AS had_A,
     hdcodds.HG AS hdc,
     CAST(SPLIT(hdcodds.H, '@')[OFFSET(1)] AS FLOAT64) AS hdc_H,
-    CAST(SPLIT(hdcodds.A, '@')[OFFSET(1)] AS FLOAT64) AS hdc_A,
-    CAST(odds_had.venue IS NULL AS INT64) AS home_adv
+    CAST(SPLIT(hdcodds.A, '@')[OFFSET(1)] AS FLOAT64) AS hdc_A
   FROM `hkjc.odds_had` odds_had
+  JOIN `master.leagues` leagues ON odds_had.tournament.tournamentShortName = leagues.hkjc_id
+  LEFT JOIN `solver.leagues` league_solver ON leagues.division = league_solver.division
+    AND leagues.type = REGEXP_EXTRACT(league_solver._FILE_NAME, r".*/(.*)\.json")
+  JOIN `master.teams` home_teams ON odds_had.homeTeam.teamID = home_teams.hkjc_id
+  LEFT JOIN `solver.teams` home_solver ON home_teams.solver_id = home_solver.id
+  JOIN `master.teams` away_teams ON odds_had.awayTeam.teamID = away_teams.hkjc_id
+  LEFT JOIN `solver.teams` away_solver ON away_teams.solver_id = away_solver.id
   LEFT JOIN `hkjc.odds_hdc` odds_hdc ON odds_had.matchID = odds_hdc.matchID
   WHERE odds_had.matchState = 'PreEvent' 
   ),
@@ -22,82 +32,36 @@ WITH
   footystats AS (
   SELECT
     matches.id,
-    TIMESTAMP_SECONDS(date_unix) AS matchDate,
+    date_unix,
     leagues.transfermarkt_id AS league_transfermarkt_id,
-    home_teams.hkjc_id AS home_hkjc_id,
-    home_teams.name AS home_name,
     home_teams.transfermarkt_id AS home_transfermarkt_id,
-    home_teams.country AS home_country,
-    away_teams.hkjc_id AS away_hkjc_id,
-    away_teams.name AS away_name,
+    home_teams.name AS home_name,
     away_teams.transfermarkt_id AS away_transfermarkt_id,
-    away_teams.country AS away_country,
+    away_teams.name AS away_name,
     avg_goal + home_solver.offence + away_solver.defence AS home_exp,
     avg_goal + away_solver.offence + home_solver.defence AS away_exp,
-    home_adv
-  FROM `footystats.matches` matches
-  JOIN `footystats.seasons` seasons ON competition_id = seasons.id
-  JOIN `master.leagues` leagues
-  ON seasons.country = leagues.country
-    AND seasons.name = leagues.footystats_name
-  JOIN `master.teams` home_teams ON matches.homeID = home_teams.footystats_id
-  JOIN `master.teams` away_teams ON matches.awayID = away_teams.footystats_id
-  JOIN `solver.teams` home_solver ON home_teams.solver_id = home_solver.id
-  JOIN `solver.teams` away_solver ON away_teams.solver_id = away_solver.id
-  JOIN `solver.leagues` league_solver ON leagues.division = league_solver.division
-    AND leagues.type = REGEXP_EXTRACT(league_solver._FILE_NAME, r".*/(.*)\.json")
-  WHERE matches.status = 'incomplete' 
-  ),
-
-  hkjc AS (
-  SELECT
-    matchID,
-    odds.matchDate,
-    COALESCE(footystats_home.league_transfermarkt_id, footystats_away.league_transfermarkt_id) AS league_transfermarkt_id,
-    COALESCE(footystats_home.home_name, footystats_away.away_name, home_hkjc_name) AS home_name,
-    COALESCE(footystats_home.home_transfermarkt_id, footystats_away.away_transfermarkt_id) AS home_transfermarkt_id,
-    COALESCE(footystats_home.away_name, footystats_away.home_name, away_hkjc_name) AS away_name,
-    COALESCE(footystats_home.away_transfermarkt_id, footystats_away.home_transfermarkt_id) AS away_transfermarkt_id,
-    had_H,
-    had_D,
-    had_A,
-    hdc,
-    hdc_H,
-    hdc_A,
-    COALESCE(footystats_home.home_exp + footystats_home.home_adv * odds.home_adv, footystats_away.away_exp + footystats_away.home_adv * odds.home_adv) AS home_exp,
-    COALESCE(footystats_home.away_exp - footystats_home.home_adv * odds.home_adv, footystats_away.home_exp - footystats_away.home_adv * odds.home_adv) AS away_exp,
-  FROM odds
-  LEFT JOIN footystats footystats_home
-  ON footystats_home.matchDate BETWEEN TIMESTAMP_SUB(odds.matchDate, INTERVAL 1 HOUR) AND TIMESTAMP_ADD(odds.matchDate, INTERVAL 1 HOUR)
-    AND odds.home_hkjc_id = footystats_home.home_hkjc_id
-    AND odds.away_hkjc_id = footystats_home.away_hkjc_id
-  LEFT JOIN footystats footystats_away
-  ON footystats_away.matchDate BETWEEN TIMESTAMP_SUB(odds.matchDate, INTERVAL 1 HOUR) AND TIMESTAMP_ADD(odds.matchDate, INTERVAL 1 HOUR)
-    AND odds.home_hkjc_id = footystats_away.away_hkjc_id
-    AND odds.away_hkjc_id = footystats_away.home_hkjc_id
-  ),
-
-  non_hkjc AS (
-  SELECT
-    CAST(footystats.id AS STRING) AS matchID,
-    matchDate,
-    league_transfermarkt_id,
-    home_name,
-    home_transfermarkt_id,
-    away_name,
-    away_transfermarkt_id,
+    home_adv,
     NULL AS had_H,
     NULL AS had_D,
     NULL AS had_A,
     CAST(NULL AS STRING) AS hdc,
     NULL AS hdc_H,
     NULL AS hdc_A,
-    home_exp + home_adv AS home_exp,
-    away_exp - home_adv AS away_exp
-  FROM footystats
-  WHERE TIMESTAMP_DIFF(matchDate, CURRENT_TIMESTAMP(), DAY) <= 5
-    AND ( home_country = 'Hong Kong'
-      OR away_country = 'Hong Kong' )
+  FROM `footystats.matches` matches
+  JOIN `footystats.seasons` seasons ON competition_id = seasons.id
+  JOIN `master.leagues` leagues
+  ON seasons.country = leagues.country
+    AND seasons.name = leagues.footystats_name
+  JOIN `solver.leagues` league_solver ON leagues.division = league_solver.division
+    AND leagues.type = REGEXP_EXTRACT(league_solver._FILE_NAME, r".*/(.*)\.json")
+  JOIN `master.teams` home_teams ON matches.homeID = home_teams.footystats_id
+  JOIN `solver.teams` home_solver ON home_teams.solver_id = home_solver.id
+  JOIN `master.teams` away_teams ON matches.awayID = away_teams.footystats_id
+  JOIN `solver.teams` away_solver ON away_teams.solver_id = away_solver.id
+  WHERE matches.status = 'incomplete'
+    AND date_unix <= UNIX_SECONDS(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL 5 DAY))
+    AND ( home_teams.country = 'Hong Kong'
+      OR away_teams.country = 'Hong Kong' ) 
   ),
 
   matches AS (
@@ -105,37 +69,37 @@ WITH
     matchID,
     matchDate,
     league_transfermarkt_id,
-    home_name,
     home_transfermarkt_id,
-    away_name,
+    home_name,
     away_transfermarkt_id,
+    away_name,
+    GREATEST(home_exp + home_adv, 0.2) AS home_exp,
+    GREATEST(away_exp - home_adv, 0.2) AS away_exp,
     had_H,
     had_D,
     had_A,
     hdc,
     hdc_H,
-    hdc_A,
-    GREATEST(home_exp, 0.2) AS home_exp,
-    GREATEST(away_exp, 0.2) AS away_exp
+    hdc_A
   FROM hkjc
   UNION ALL
   SELECT
-    matchID,
-    matchDate,
+    CAST(footystats.id AS STRING) AS matchID,
+    TIMESTAMP_SECONDS(date_unix) AS matchDate,
     league_transfermarkt_id,
-    home_name,
     home_transfermarkt_id,
-    away_name,
+    home_name,
     away_transfermarkt_id,
+    away_name,
+    GREATEST(home_exp + home_adv, 0.2) AS home_exp,
+    GREATEST(away_exp - home_adv, 0.2) AS away_exp,
     had_H,
     had_D,
     had_A,
     hdc,
     hdc_H,
-    hdc_A,
-    GREATEST(home_exp, 0.2) AS home_exp,
-    GREATEST(away_exp, 0.2) AS away_exp
-  FROM non_hkjc
+    hdc_A
+  FROM footystats
   ),
 
   match_probs AS (
