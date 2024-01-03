6047b33ca7daf5e8a2fa9635cf778f9ff299c583
Jamie Tan <tanjt107@gmail.com>
2024-01-03 16:19:49 +0800

    Use partitioned key for joining

--- bigquery/routine/simulation/get_teams.sql
+++ bigquery/routine/simulation/get_teams.sql
@@ -3,7 +3,7 @@ SELECT
     offence,
     defence
 FROM `${project_id}.master.leagues` leagues
-JOIN `${project_id}.footystats.teams` footystats ON leagues.latest_season_id = footystats.competition_id
+JOIN `${project_id}.footystats.teams` footystats ON leagues.latest_season_id = footystats._SEASON_ID
 JOIN `${project_id}.master.teams` master_teams ON footystats.id = master_teams.footystats_id
 JOIN `${project_id}.solver.teams_latest` solver ON master_teams.solver_id = solver.id
 WHERE leagues.footystats_id = league
