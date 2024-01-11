20716f33e12772daa732ad2e13bd79ced01ae480
Jamie Tan <tanjt107@gmail.com>
2024-01-11 21:32:34 +0800

    Refactor query

--- bigquery/routine/outputs/team_ratings_club.sql
+++ bigquery/routine/outputs/team_ratings_club.sql
@@ -6,9 +6,9 @@ WITH latest AS (
     id,
     leagues.transfermarkt_id As league_transfermarkt_id,
     leagues.name AS league_name,
-    ROUND(offence, 2) AS offence,
-    ROUND(defence, 2) AS defence,
-    ROUND(ratings.rating, 1) AS rating,
+    offence,
+    defence,
+    ratings.rating,
     _TYPE,
     _DATE_UNIX
   FROM `solver.team_ratings` ratings
@@ -27,11 +27,11 @@ SELECT
   team_name,
   league_transfermarkt_id,
   league_name,
-  offence,
-  defence,
-  latest.rating,
+  ROUND(offence, 2) AS offence,
+  ROUND(defence, 2) AS defence,
+  ROUND(latest.rating, 1) AS rating,
   FORMAT_TIMESTAMP('%F %H:%M', TIMESTAMP_ADD(TIMESTAMP_SECONDS(_DATE_UNIX), INTERVAL 2 HOUR), 'Asia/Hong_Kong') AS date_unix
 FROM latest
-JOIN `solver.team_ratings_7d` _7d ON latest.id = _7d.id
+LEFT JOIN `solver.team_ratings_7d` _7d ON latest.id = _7d.id
   AND latest._TYPE = _7d._TYPE
 ORDER BY rank;
