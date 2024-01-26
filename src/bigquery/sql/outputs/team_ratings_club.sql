77ae7e0018c2180264335aea266e35b6c275b48d
Jamie Tan <tanjt107@gmail.com>
2024-01-26 09:10:43 +0800

    Minor fix

--- src/bigquery/sql/outputs/team_ratings_club.sql
+++ src/bigquery/sql/outputs/team_ratings_club.sql
@@ -1,6 +1,6 @@
 WITH latest AS (
   SELECT
-    RANK() OVER(ORDER BY ratings.rating DESC) AS rank,
+    RANK() OVER(ORDER BY team_ratings.rating DESC) AS rank,
     teams.transfermarkt_id AS team_transfermarkt_id,
     teams.name AS team_name,
     id,
@@ -8,13 +8,13 @@ WITH latest AS (
     leagues.name AS league_name,
     offence,
     defence,
-    ratings.rating,
+    team_ratings.rating,
     _TYPE,
     _DATE_UNIX
-  FROM `solver.team_ratings` ratings
-  JOIN `master.teams` teams ON ratings.id = teams.solver_id
+  FROM solver.team_ratings
+  JOIN master.teams ON team_ratings.id = teams.solver_id
     AND team_ratings._TYPE = teams.type
-  JOIN `master.leagues` leagues ON teams.league_name = leagues.name
+  JOIN master.leagues ON teams.league_name = leagues.name
   WHERE (leagues.hkjc_id IS NOT NULL
       OR leagues.is_manual)
     AND team_ratings._TYPE = 'Club'
@@ -22,7 +22,7 @@ WITH latest AS (
 
 SELECT
   rank,
-  RANK() OVER(ORDER BY _7d.rating DESC) - rank AS rank_7d_diff,
+  RANK() OVER(ORDER BY team_ratings_7d.rating DESC) - rank AS rank_7d_diff,
   team_transfermarkt_id,
   team_name,
   league_transfermarkt_id,
@@ -32,6 +32,6 @@ SELECT
   ROUND(latest.rating, 1) AS rating,
   FORMAT_TIMESTAMP('%F %H:%M', TIMESTAMP_ADD(TIMESTAMP_SECONDS(_DATE_UNIX), INTERVAL 2 HOUR), 'Asia/Hong_Kong') AS date_unix
 FROM latest
-LEFT JOIN `solver.team_ratings_7d` _7d ON latest.id = _7d.id
-  AND latest._TYPE = _7d._TYPE
+LEFT JOIN solver.team_ratings_7d ON latest.id = team_ratings_7d.id
+  AND latest._TYPE = team_ratings_7d._TYPE
 ORDER BY rank;
