6a237b942a6ea7dab3fbd8df7d5f0b9f240b704f
Jamie Tan <tanjt107@gmail.com>
2024-02-24 04:17:50 +0800

    Improve delta download logic for xg update

--- src/bigquery/sql/footystats/get_season_id_delta.sql
+++ src/bigquery/sql/footystats/get_season_id_delta.sql
@@ -4,5 +4,7 @@ SELECT
   _COUNTRY AS country,
   _YEAR AS year
 FROM `${project_id}.footystats.matches`
-WHERE status = 'incomplete'
-  AND date_unix < UNIX_SECONDS(CURRENT_TIMESTAMP())
+WHERE 
+  date_unix > UNIX_SECONDS(TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL -3 DAY))
+  AND ((status = 'incomplete' AND date_unix < UNIX_SECONDS(CURRENT_TIMESTAMP()))
+    OR (status = 'complete' AND total_xg = 0))
