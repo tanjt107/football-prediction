ec014483529232a648d57989de40fd22e3bb1f96
Jamie Tan <tanjt107@gmail.com>
2024-03-11 08:51:31 +0800

    perf: update download frequency logic

--- src/bigquery/sql/solver/get_latest_match_date.sql
+++ src/bigquery/sql/solver/get_latest_match_date.sql
@@ -1,6 +1,18 @@
 SELECT
     MAX(date_unix) AS max_date_unix
 FROM ${project_id}.footystats.matches
-JOIN ${project_id}.simulation.params ON matches._NAME = params.league
-WHERE status = 'complete'
-    AND type = 'Club'
+JOIN `${project_id}.master.teams` home_teams ON matches.homeID = home_teams.footystats_id
+JOIN `${project_id}.master.teams` away_teams ON matches.awayID = away_teams.footystats_id
+JOIN ${project_id}.master.leagues ON matches._NAME = leagues.footystats_name
+WHERE leagues.type = 'Club'
+    AND status = 'complete'
+    AND (EXISTS (
+        SELECT 1
+        FROM ${project_id}.simulation.params
+        WHERE home_teams.league_name = params.league
+            OR away_teams.league_name = params.league
+            OR leagues.footystats_name = params.league )
+        OR EXISTS (
+            SELECT 1
+            FROM ${project_id}.manual.non_hkjc_leagues
+            WHERE leagues.footystats_name = non_hkjc_leagues.footystats_id ))
