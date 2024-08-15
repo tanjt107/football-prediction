5a3b61f4584cc49a33b6bb3bef9e8fcf23045531
Jamie Tan <tanjt107@gmail.com>
2024-08-15 12:09:54 +0800

    feat: change team ratings formula

--- src/bigquery/sql/solver/team_ratings.sql
+++ src/bigquery/sql/solver/team_ratings.sql
@@ -1,26 +1,19 @@
-WITH maxmin AS (
+WITH match_probs AS (
   SELECT
-    _TYPE,
-    MAX(offence - defence) AS _max,
-    MIN(offence - defence) AS _min
+  id,
+  _TYPE,
+  GREATEST(1.35 + offence, 0.2) AS offence,
+  GREATEST(1.35 + defence, 0.2) AS defence,
+  functions.matchProbs(GREATEST(1.35 + offence, 0.2), GREATEST(1.35 + defence, 0.2), '0') AS had_probs,
+  _DATE_UNIX
   FROM solver.teams_latest
-  JOIN master.teams ON teams_latest.id = teams.solver_id
-    AND teams_latest._TYPE = teams.type
-  WHERE hkjc_id IS NOT NULL
-    OR is_manual
-  GROUP BY _TYPE
 )
 
 SELECT
   id,
-  teams_latest._TYPE,
-  GREATEST(1.35 + offence, 0.2) AS offence,
-  GREATEST(1.35 + defence, 0.2) AS defence,
-  CASE
-    WHEN offence - defence > 0 THEN (offence - defence) / _max * 50 + 50
-    WHEN offence - defence < 0 THEN (1 - (offence - defence) / _min) * 50
-    ELSE 50
-  END AS rating,
+  _TYPE,
+  offence,
+  defence,
+  (had_probs[0] * 3 + had_probs[1]) / 3 * 100 AS rating,
   _DATE_UNIX
-FROM solver.teams_latest
-JOIN maxmin USING (_TYPE)
+FROM match_probs
--- src/bigquery/sql/solver/team_ratings_7d.sql
+++ src/bigquery/sql/solver/team_ratings_7d.sql
@@ -1,5 +1,14 @@
+WITH match_probs AS (
+  SELECT
+  id,
+  _TYPE,
+  functions.matchProbs(GREATEST(1.35 + offence, 0.2), GREATEST(1.35 + defence, 0.2), '0') AS had_probs,
+  _DATE_UNIX
+  FROM solver.teams_7d
+)
+
 SELECT
   id,
   _TYPE,
-  offence - defence AS rating
-FROM `solver.teams_7d`
+  (had_probs[0] * 3 + had_probs[1]) / 3 * 100 AS rating
+FROM match_probs
