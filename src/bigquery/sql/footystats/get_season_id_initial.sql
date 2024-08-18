c7e0fed233bfc426e410aedc8aeb9cad5aa18dab
Jamie Tan <tanjt107@gmail.com>
2024-08-18 11:54:19 +0800

    feat: reduce number of seasons during initial load

--- src/bigquery/sql/footystats/get_season_id_initial.sql
+++ src/bigquery/sql/footystats/get_season_id_initial.sql
@@ -3,5 +3,6 @@ SELECT
   season.id AS season_id,
   season.country,
   season.year
-FROM ${project_id}.footystats.league_list,
+FROM ${project_id}.footystatsaway_team_in_rating,
   league_list.season
+WHERE CAST(RIGHT(season.year, 4) AS INT) >= EXTRACT(YEAR FROM CURRENT_DATE()) - 5
