340f1dc06882fab3dcc39f891838dae8cd998be2
Jamie Tan <tanjt107@gmail.com>
2024-08-18 12:11:40 +0800

    fix: typo

--- src/bigquery/sql/footystats/get_season_id_initial.sql
+++ src/bigquery/sql/footystats/get_season_id_initial.sql
@@ -3,6 +3,6 @@ SELECT
   season.id AS season_id,
   season.country,
   season.year
-FROM ${project_id}.footystatsaway_team_in_rating,
+FROM ${project_id}.footystats.league_list,
   league_list.season
 WHERE CAST(RIGHT(season.year, 4) AS INT) >= EXTRACT(YEAR FROM CURRENT_DATE()) - 5
