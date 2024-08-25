f22f0ab34d9e00194abf59ddca61cf24208663ee
Jamie Tan <tanjt107@gmail.com>
2024-08-25 23:00:36 +0800

    fix: use odds latest table for map tournaments

--- src/bigquery/sql/operations/map_hkjc_tournaments.sql
+++ src/bigquery/sql/operations/map_hkjc_tournaments.sql
@@ -1,6 +1,6 @@
 SELECT MAX(kick_off_time) AS kick_off_time, tournament_id, tournament_name
-FROM hkjc.odds_today
-LEFT JOIN master.leagues ON odds_today.tournament_id = leagues.hkjc_id
+FROM hkjc.odds_last
+LEFT JOIN master.leagues ON odds_last.tournament_id = leagues.hkjc_id
 WHERE leagues.transfermarkt_id IS NULL
-  AND tournament_id NOT in ('CUP', 'OLW')
+  AND tournament_id NOT in ('CUP', 'OLW', 'CLB', 'INT')
 GROUP BY tournament_id, tournament_name
