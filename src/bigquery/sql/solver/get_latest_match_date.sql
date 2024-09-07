546149e077832c962108656ebcb49f2f88506f0a
Jamie Tan <tanjt107@gmail.com>
2024-09-07 11:06:09 +0800

    fix: removed simulation.params

--- src/bigquery/sql/solver/get_latest_match_date.sql
+++ src/bigquery/sql/solver/get_latest_match_date.sql
@@ -7,10 +7,6 @@ JOIN ${project_id}.master.leagues ON matches._NAME = leagues.footystats_name
 WHERE leagues.type = _type
     AND status = 'complete'
     AND date_unix < UNIX_SECONDS(CURRENT_TIMESTAMP())
-    AND (EXISTS (
-        SELECT 1
-        FROM ${project_id}.simulation.params
-        WHERE home_teams.league_name = params.league
-            OR away_teams.league_name = params.league
-            OR leagues.footystats_name = params.league )
-        OR leagues.is_manual)
+    AND (home_teams.is_simulate
+      OR away_teams.is_simulate
+      OR leagues.is_simulate)
