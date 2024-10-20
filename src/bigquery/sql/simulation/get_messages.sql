a83bf1c54d52b66ec501aa8b1de617a259979d83
Jamie Tan <tanjt107@gmail.com>
2024-10-20 12:16:02 +0800

    chore: remove temp fix

--- src/bigquery/sql/simulation/get_messages.sql
+++ src/bigquery/sql/simulation/get_messages.sql
@@ -4,7 +4,6 @@ FROM ${project_id}.master.leagues master
 JOIN ${project_id}.footystats.matches ON master.latest_season_id = matches._SEASON_ID
 LEFT JOIN ${project_id}.simulation.leagues simulation ON master.footystats_name = simulation._LEAGUE
 WHERE status = 'complete'
-    AND TIMESTAMP_SECONDS(date_unix) < CURRENT_TIMESTAMP() -- TODO Remove after 2024/09/15
     AND is_simulate
     AND type = type
 GROUP BY footystats_name
--- src/bigquery/sql/solver/get_messages.sql
+++ src/bigquery/sql/solver/get_messages.sql
@@ -6,9 +6,8 @@ JOIN `${project_id}.master.teams` away_teams ON matches.awayID = away_teams.foot
 JOIN ${project_id}.master.leagues ON matches._NAME = leagues.footystats_name
 JOIN ${project_id}.solver.leagues solver ON leagues.type = solver._TYPE
 WHERE status = 'complete'
-    AND date_unix < UNIX_SECONDS(CURRENT_TIMESTAMP()) -- TODO Remove after 2024/09/15
     AND (home_teams.is_simulate
       OR away_teams.is_simulate
       OR leagues.is_simulate)
 GROUP BY solver._TYPE
--- HAVING latest_match_date > last_run
+HAVING latest_match_date > last_run
